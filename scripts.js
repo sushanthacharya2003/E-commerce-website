7d4e6d45eba146cfb7f6388f7c192b435a774a01
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-11-11 15:24:36 +0530

    image carousel added for products

--- scripts.js
+++ scripts.js
@@ -7,37 +7,41 @@ async function getProducts() {
         // product details
         const products = data.products;
         products.forEach(p => {
+          console.log(p.images[0]);  // first image
           console.log(p.title);      // product title
           console.log(p.price);      // price
           console.log(p.rating);     // rating
-          console.log(p.images[0]);  // first image
         });
 
         const productsContainer=document.querySelector(".products");
         products.forEach((p)=>{
             const div=document.createElement("div");
             div.classList.add("product");
-            let imgIndex=0;
             div.innerHTML=`<div class="img_con">
             <button class="prevBtn">◀</button>
-            <img src="${p.images[0]}" class="productImg" alt="productImage" width="80" >
+            <img src="${p.images[0]}" class="productImg" alt="productImage" width="80%" height="100%" >
+            <img src="${p.thumbnail}" class="productThumb" alt="productThumbnail" width="80%" height="100%" style="display:none" >
             <button class="nextBtn">▶</button>
             </div>
             <h3>${p.title}</h3>
             <p>${p.price}</p>`
-            // ✅ SLIDER LOGIC (per product)
-            const imgTag = div.querySelector(".productImg");
+
+            console.log(p.images);
+            console.log("Thumb:", p.thumbnail);
+
             const prev = div.querySelector(".prevBtn");
             const next = div.querySelector(".nextBtn");
+            const img = div.querySelector(".productImg");
+            const thumb = div.querySelector(".productThumb");
 
             prev.addEventListener("click", () => {
-                imgIndex = (imgIndex - 1 + p.images.length) % p.images.length;
-                imgTag.src = p.images[imgIndex];
+                img.style.display = "block";
+                thumb.style.display = "none";
             });
 
             next.addEventListener("click", () => {
-                imgIndex = (imgIndex + 1) % p.images.length;
-                imgTag.src = p.images[imgIndex];
+                img.style.display = "none";
+                thumb.style.display = "block";
             });
 
             productsContainer.appendChild(div);
