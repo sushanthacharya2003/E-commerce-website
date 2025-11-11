cff1ab560f3751cb40aa230c88678c52b59f92b7
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-11-11 12:51:38 +0530

    added displaying each product details functionality

--- scripts.js
+++ scripts.js
@@ -1,16 +1,50 @@
 //Implement your code here to make it a functional shopping website
-    async function getProducts() {
+
+// fetching the api
+async function getProducts() {
         const res = await fetch("https://dummyjson.com/products")
-        const data = await data.json;
-        return data.products;
+        const data = await res.json();
+        // product details
+        const products = data.products;
+        products.forEach(p => {
+          console.log(p.title);      // product title
+          console.log(p.price);      // price
+          console.log(p.rating);     // rating
+          console.log(p.images[0]);  // first image
+        });
+        return products;
+
+        const productsContainer=document.querySelector(".products");
+        products.forEach((p)=>{
+            const div=document.createElement("div");
+            div.classList.add("product");
+            let imgIndex=0;
+            div.innerHTML=`<div class="img_con">
+            <button class="prevBtn"><<<button/>
+            <img src="${p.images[0]}" alt="productImg" width="80" >
+            <button class="prevBtn">>><button/>
+            </div><h3>${p.title}</h3>
+            <p>${price}</p>`
+            // ✅ SLIDER LOGIC (per product)
+            const imgTag = div.querySelector(".productImg");
+            const prev = div.querySelector(".prevBtn");
+            const next = div.querySelector(".nextBtn");
+
+            prev.addEventListener("click", () => {
+              imgIndex = (imgIndex - 1 + p.images.length) % p.images.length;
+              imgTag.src = p.images[imgIndex];
+            });
+
+            next.addEventListener("click", () => {
+              imgIndex = (imgIndex + 1) % p.images.length;
+              imgTag.src = p.images[imgIndex];
+            });
+
+            productsContainer.appendChild(div);
+
+        })
+
     }
 
-const id=1;
-const product=document.querySelector(".products");
-function displayProducts(){
-    const products=await getProducts();
-    products.id.forEach(element => {
-
-    });
-}
+// getProducts();
 
